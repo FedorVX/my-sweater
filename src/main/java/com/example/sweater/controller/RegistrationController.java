9b6868039b92c7f45375f0add90fddf7c95c85d7
Fedor <valayev2005@gmail.com>
2025-05-16 20:27:51 +0700

    ервый коммит

---
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,48 @@
+package com.example.sweater.controller;
+
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+
+    @ModelAttribute
+    public void addCsrfToken(CsrfToken token, Map<String, Object> model) {
+        model.put("_csrf", token);  // чтобы Mustache мог использовать {{_csrf.token}}
+    }
+
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if(userFromDb!=null){
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+        return "redirect:/login";
+    }
+}
